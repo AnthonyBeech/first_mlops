fb83423b6ca0178fee7118a4b3c83a6d0e272a97
AnthonyBeech <abeech123@virginmedia.com>
2023-12-27 12:56:31 +0000

    add simple test for train

--- iris_classification.py
+++ iris_classification.py
@@ -1,8 +1,13 @@
-import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+
+
+def train_and_evaluate_model(clf, X_train, y_train):
+    clf.fit(X_train, y_train)
+    return clf
+    
 
 # Load the Iris dataset
 iris = datasets.load_iris()
@@ -15,11 +20,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 clf = RandomForestClassifier(random_state=42)
 
 # Train the model
-clf.fit(X_train, y_train)
+clf = train_and_evaluate_model(clf, X_train, y_train)
 
 # Make predictions on the test data
 y_pred = clf.predict(X_test)
 
 # Calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
+
 print(f"Accuracy: {accuracy * 100:.2f}%")
---
+++ iris_test.py
@@ -0,0 +1,33 @@
+import unittest
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from iris_classification import train_and_evaluate_model  # Replace 'your_script' with the actual module name
+
+class TestRandomForestClassifier(unittest.TestCase):
+    def test_model_accuracy(self):
+        # Load the Iris dataset
+        iris = datasets.load_iris()
+        X, y = iris.data, iris.target
+
+        # Split the dataset into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        # Create a Random Forest classifier
+        clf = RandomForestClassifier(random_state=42)
+
+        # Train the model using the function from your script
+        trained_model = train_and_evaluate_model(clf, X_train, y_train, X_test, y_test)
+
+        # Make predictions on the test data
+        y_pred = trained_model.predict(X_test)
+
+        # Calculate accuracy
+        accuracy = accuracy_score(y_test, y_pred)
+
+        # Assert that the accuracy is greater than or equal to 0.9 (90%)
+        self.assertGreaterEqual(accuracy, 0.9)
+
+if __name__ == '__main__':
+    unittest.main()
